79bb31dacb071a41fc3febefc7d317004a393e41
yosima050 <byosep050@gmail.com>
2024-09-27 09:57:16 +0700

    Revision2

--- Kafe26.java
+++ Kafe26.java
@@ -42,6 +42,9 @@ public class Kafe26 {
             case 'L':
                 totalHarga += 0.4 * totalHarga;
                 break;
+            default:
+                System.out.println("Ukuran Cup Tidak Valid");
+                return;
         }
 
         double diskon = keanggotaan ? 0.1 : 0;
